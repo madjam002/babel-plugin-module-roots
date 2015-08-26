90e69dd8d072d2220e23d56c493289960271ea8a
Jamie Greeff <madjam002@yahoo.co.uk>
2015-08-26 22:41:03 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import fs from 'fs'
+import path from 'path'
+
+const PLUGIN_NAME = 'module-roots'
+
+export default function({ Plugin, types: t }) {
+
+  function mapModule(context, mod) {
+    let { paths, extensions } = context.state.opts.extra[PLUGIN_NAME] || {}
+
+    if (paths == null) {
+      paths = ['./src']
+    }
+
+    if (extensions == null) {
+      extensions = ['.js', '/index.js']
+    }
+
+    for (const p of paths) {
+      const potentialPath = path.join(p, mod)
+
+      // check if potential path exists
+      for (const ext of extensions) {
+        const potentialPathWithExt = potentialPath + ext
+
+        if (fs.existsSync(potentialPathWithExt)) {
+          return path.relative(path.dirname(context.state.opts.filename), potentialPath)
+        }
+      }
+    }
+  }
+
+  function transformImportCall(context, call) {
+    const mod = call.source.value
+
+    const newModule = mapModule(context, mod)
+    if (newModule) {
+      return t.importDeclaration(
+        call.specifiers,
+        t.literal(newModule),
+      )
+    }
+  }
+
+  return new Plugin(PLUGIN_NAME, {
+    visitor: {
+      ImportDeclaration: {
+        exit(node) {
+          return transformImportCall(this, node)
+        }
+      }
+    }
+  })
+
+}
